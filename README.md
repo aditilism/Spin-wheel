c51a5cd19a366fa8f55a46c16e57346955e0036b
aditi Indre <indreaditi@gmail.com>
2024-07-18 21:14:17 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Spin-wheel
+This is a JavaScript Application
+Description
+This JavaScript application implements a dynamic spin wheel using Chart.js for interactive data visualization. The spin wheel is designed to facilitate decision-making scenarios, such as choosing between "Heads" or "Tails," or any custom options defined by the user. Users can spin the wheel and receive outcomes based on predefined angles and values.
+
+Features
+Dynamic Spinning Animation: The spin wheel provides an engaging experience with adjustable rotation speeds, enhancing user interaction.
+
+Outcome Display: When the spin wheel stops spinning, it displays the selected outcome ("Heads," "Tails") accompanied by a congratulatory message 
+Customizable Design: Each segment of the spin wheel can be customized with different colors and sizes using Chart.js, allowing for a visually appealing and cohesive design that matches your application's theme.
+
+Responsive and Interactive: The application is responsive across different devices and browsers, ensuring a seamless user experience.
---
+++ script.js
@@ -0,0 +1,83 @@
+/* --------------- Spin Wheel  --------------------- */
+const spinWheel = document.getElementById("spinWheel");
+const spinBtn = document.getElementById("spin_btn");
+const text = document.getElementById("text");
+/* --------------- Minimum And Maximum Angle For A value  --------------------- */
+const spinValues = [
+  { minDegree: 0, maxDegree: 90, value:"Tails"  },
+  { minDegree: 91, maxDegree: 135, value: "Heads" },
+  { minDegree: 136, maxDegree: 240, value: "Tails "},
+  { minDegree: 341, maxDegree: 360, value:"Heads" },
+];
+/* --------------- Size Of Each Piece  --------------------- */
+const size = [10, 10, 10, 10];
+/* --------------- Background Colors  --------------------- */
+var spinColors = [
+  "#E74C3C",
+  "#7D3C98",
+  "#2E86C1",
+  "#138D75",
+];
+/* --------------- Chart --------------------- */
+/* --------------- Guide : https://chartjs-plugin-datalabels.netlify.app/guide/getting-started.html --------------------- */
+let spinChart = new Chart(spinWheel, {
+  plugins: [ChartDataLabels],
+  type: "pie",
+  data: {
+    labels: ["Tails","Heads","Tails","Heads" ],
+    datasets: [
+      {
+        backgroundColor: spinColors,
+        data: size,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    animation: { duration: 0 },
+    plugins: {
+      tooltip: false,
+      legend: {
+        display: false,
+      },
+      datalabels: {
+        rotation: 90,
+        color: "#ffffff",
+        formatter: (_, context) => context.chart.data.labels[context.dataIndex],
+        font: { size: 26 },
+      },
+    },
+  },
+});
+/* --------------- Display Value Based On The Angle --------------------- */
+const generateValue = (angleValue) => {
+  for (let i of spinValues) {
+    if (angleValue >= i.minDegree && angleValue <= i.maxDegree) {
+      text.innerHTML = `<p>Congratulations, You are Team ${i.value} !! </p>`;
+      spinBtn.disabled = false;
+      break;
+    }
+  }
+};
+/* --------------- Spinning Code --------------------- */
+let count = 0;
+let resultValue = 101;
+spinBtn.addEventListener("click", () => {
+  spinBtn.disabled = true;
+  text.innerHTML = `<p>Try Your Luck!</p>`;
+  let randomDegree = Math.floor(Math.random() * (355 - 0 + 1) + 0);
+  let rotationInterval = window.setInterval(() => {
+    spinChart.options.rotation = spinChart.options.rotation + resultValue;
+    spinChart.update();
+    if (spinChart.options.rotation >= 360) {
+      count += 1;
+      resultValue -= 5;
+      spinChart.options.rotation = 0;
+    } else if (count > 15 && spinChart.options.rotation == randomDegree) {
+      generateValue(randomDegree);
+      clearInterval(rotationInterval);
+      count = 0;
+      resultValue = 101;
+    }
+  }, 10);
+});
